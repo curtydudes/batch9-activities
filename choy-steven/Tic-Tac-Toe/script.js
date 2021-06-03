296fd8f738cf2d13389be58bf0dbd5d89c91c81d
Steven Choy <stevenchoy0915@gmail.com>
2021-06-03 14:15:26 +0800

    TTC project finished

--- choy-steven/Tic-Tac-Toe/script.js
+++ choy-steven/Tic-Tac-Toe/script.js
@@ -1,93 +1,223 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
-const cellElements = document.querySelectorAll("[data-cell]")
-const winningMessageElement = document.getElementById("winningMessage")
-const winningMessageTextElement = document.querySelector("[data-winning-message-text]")
-const restartButton = document.getElementById('restartButton')
-const board = document.getElementById("board")
-let circleTurn;
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+    const control = document.querySelector('.controls');
+    const previousBtn = document.querySelector('#previous');
+    const nextBtn = document.querySelector('#next');
+    const modalContainer = document.querySelector('.modal-bg');
+    // const modalHeader = document.querySelector('#modal-header');
+    // const modalText = document.querySelector('#modal-text');
+    const newGameBtn = document.querySelector('#newBtn');
+    const gameHistoryBtn = document.querySelector('#historyBtn');
 
-startGame()
+    //create an array with 9 empty strings and this will act as a board
+    let board = ['', '', '', '', '', '', '', '', ''];
+    // store the current player
+    let currentPlayer = 'X';
+    // if game is resolved or not
+    // this assumes that whenever board has a space or no one has one, it stays true
+    let isGameActive = true;
 
-restartButton.addEventListener('click', startGame)
+    //create arrays for previous and next button
+    let previousMoveArray = [];
+    let nextMoveArray = [];
+    
 
-function startGame () {
-    circleTurn = false
-    cellElements.forEach(cell => {
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(CIRCLE_CLASS)
-        cell.removeEventListener('click', handleClick)
-        cell.addEventListener("click", handleClick, {once:true})
+    //see end game state
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+
+    /*
+        Indexes within the board
+        [0] [1] [2]
+        [3] [4] [5]
+        [6] [7] [8]
+    */
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+
+
+
+
+    // loop function to always check if TTC has a winner
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            let a = board[winCondition[0]];
+            let b = board[winCondition[1]];
+            let c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+        //if roundWon is true
+    if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            // and game is not active
+            // return announce winner
+            isGameActive = false;
+            return;
+        }
+        // if the board does not have any more spaces, it's a tie
+    if (!board.includes(''))
+        announce(TIE);
+    }
+    //announce the end game winner via HTML
+    const announce = (type) => {
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+        modalContainer.style.display = 'flex';
+    };
+    //check if the tile has a value already
+    // makes sure players only play empty tile
+    const isValidAction = (tile) => {
+        if (tile.innerText === 'X' || tile.innerText === 'O'){
+            return false;
+        }
+
+        return true;
+    };
+
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        // The conditional operator assigns a value to a variable based on a condition.
+        // choose between x or o then show in html
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+    // function called when user clicks on a tile
+    //  valid action will check if game is active
+    // if all action is valid, will assign to player x or o class based on player
+    // currentPlayer will depend will come from changePlayer function that changes X to O
+    // update board will check via index if we have a winner or not 
+
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+        } 
+            let moveObject = {};
+            // index will call the number
+            moveObject.tile = index;
+            // currentPlayer will call the X or O turn
+            moveObject.player = currentPlayer;
+            // tile will call X and O as a string in the grid
+            moveObject.move = tile;
+            
+            console.log(moveObject)
+
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+            previousMoveArray.push(moveObject); 
+        
+    }
+
+    //previous button
+    previousBtn.addEventListener('click', () => {
+        nextBtn.style.visibility = 'visible';
+        if (previousMoveArray.length != 0) {
+            let lastMove = previousMoveArray[previousMoveArray.length - 1]; //move object
+            let newTile = lastMove.move;
+            newTile.innerHTML = "";
+            nextMoveArray.push(lastMove);
+            previousMoveArray.pop();
+            console.log(previousMoveArray);
+            console.log(lastMove);
+        } else {
+            previousBtn.style.visibility = 'hidden';
+        }
+    });
+
+    //next button
+    nextBtn.addEventListener('click', () => {
+        previousBtn.style.visibility = 'visible';
+        if (nextMoveArray.length != 0) {
+            let nextMove = nextMoveArray[nextMoveArray.length - 1]; //moveObject
+            // console.log(nextMove);
+            let newTile = nextMove.move;
+            let nextIndex = nextMove.player;
+            newTile.innerHTML = nextIndex;
+            previousMoveArray.push(nextMove);
+            nextMoveArray.pop();
+            console.log(nextMove);
+            console.log(previousMoveArray);
+        } else {
+            nextBtn.style.visibility = 'hidden';
+            previousBtn.style.visibility = 'visible';
+        }
     })
-    setBoardHoverClass()
-    winningMessageElement.classList.remove('show')
-}
 
-function handleClick(e) {
-    const cell = e.target
-    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-    placeMark(cell, currentClass)
+    gameHistoryBtn.addEventListener('click', () => {
+    previousBtn.style.visibility = 'visible';
+    modalContainer.style.display = 'none';
+    });
 
-    //check for win
-    if (checkWin(currentClass) ) {
-        endGame(false)
-    } else if (isDraw()) {
-        endGame(true)
-    } else {
-        swapTurns()
-        setBoardHoverClass()
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+        control.classList.add('hide');
+
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+//every tile will be empty and remove any classList 
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        });
     }
-}
+    //attach an event listener to every tile 
+    //every time a tile will be clicked, userAction will take place 
+    // and get the tile and index
 
-function endGame(draw){
-    if (draw) {
-        winningMessageTextElement.innerText = 'Draw!'
-    } else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's":"X's"} Wins!`
-    }
-    winningMessageElement.classList.add('show')
-}
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+    // resetButton
+    resetButton.addEventListener('click', resetBoard);
+    // modal button
+    newGameBtn.addEventListener('click', resetBoard);
+});
 
-function isDraw() {
-    return [...cellElements].every(cell => {
-        return cell.classList.contains(X_CLASS) || 
-        cell.classList.contains(CIRCLE_CLASS)
-    })
-}
+previousBtn.style.visibility = 'hidden';
+nextBtn.style.visibility = 'hidden';
 
 
-function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass)
-}
 
-function swapTurns(){
-    circleTurn = !circleTurn
-}
-
-function setBoardHoverClass () {
-    board.classList.remove(X_CLASS)
-    board.classList.remove(CIRCLE_CLASS)
-    if (circleTurn){
-        board.classList.add(CIRCLE_CLASS)
-    } else {
-        board.classList.add(X_CLASS)
-    }
-}
-
-function checkWin(currentClass) {
-   return WINNING_COMBINATIONS.some(combination => {
-       return combination.every(index => {
-           return cellElements[index].classList.contains(currentClass)
-       })
-   })
-}
